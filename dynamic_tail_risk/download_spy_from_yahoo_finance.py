7b36a4e33674d4e8723e7a2d36971fd2b5e424eb
chfchen <chfchen08@gmail.com>
2026-08-19 09:06:01 -0500

    Update download_spy_from_yahoo_finance.py

--- dynamic_tail_risk/download_spy_from_yahoo_finance.py
+++ dynamic_tail_risk/download_spy_from_yahoo_finance.py
@@ -4,11 +4,10 @@ from google.colab import files
 # Define ticker symbol and date range
 ticker_symbol = "SPY"
 start_date = "1995-12-31"
-end_date = "2026-01-01"  # Set to Jan 1, 2026 to include Dec 31, 2025
+end_date = "2026-01-01"  #To include 12/31/25
 
-print(f"Fetching SPY data from {start_date} to 2025-12-31...")
+print(f"Downloading SPY data from {start_date} to 2025-12-31...")
 
-# 1. Download market data
 df = yf.download(
     ticker_symbol,
     start=start_date,
